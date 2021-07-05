2704bafd210c4708df87e6314973aead7cfb127f
Uhu_s <lukaszsowa1@gmail.com>
2021-07-05 09:17:21 +0200

    test upload

--- src/main/java/com/company/MainDAO.java
+++ src/main/java/com/company/MainDAO.java
@@ -19,7 +19,7 @@ public class MainDAO {
        User user = new User("hania@gmail.com", "hania", "mojeHaslo");
 
      userDAO.create(user);
-        System.out.println(user.getId());
+     System.out.println(user.getId());
      // userDAO.read(666);
      //userDAO.update(userDAO.read(7));
      //System.out.println(user1);
